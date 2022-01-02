da26575f7a4cc3f58eaa985079811fc7dba9e3f7
Thalya Stéffany <87552041+ThalyaSteffany-tech@users.noreply.github.com>
2022-01-02 19:22:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./Conta/ContaCorrente.js";
+import {ContaPoupanca} from "./Conta/ContaPoupanca.js";
+import { ContaSalario } from "./Conta/ContaSalario.js";
+const cliente1 = new Cliente("Ricardo", 11122233309);
+
+
+const contaCorrenteRicardo = new ContaCorrente(cliente1,1001);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(10)
+const contaPoupanca = new ContaPoupanca(50,cliente1,1001)
+const contaSalario = new ContaSalario(cliente1)
+contaSalario.depositar(200)
+contaSalario.sacar(10)
+
+console.log(contaSalario)
+
